7b6ae3fe97e37a38cb2392d581f238944e55a74c
İsmet Murat ONAY <34923740+muratonay1@users.noreply.github.com>
2022-12-17 16:26:18 +0300

    eksik parametre

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import Pocket from "./pocket-core/Pocket.js";
 import PocketService from "./pocket-core/PocketService.js";
 import PocketUtility from "./pocket-core/PocketUtility.js";
 
-PocketService.execute("GetMongoLogs",{},(response)=>{
+PocketService.execute("GetMongoLogs",{"name":"test"},(response)=>{
     console.log("response = ");
     console.log(response)
 })
